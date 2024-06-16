3d0ce26bdf5e36ac76a99e70b1955760b7476727
Jérémy Dutheil <dutheil.jeremy@gmail.com>
2024-06-16 17:52:57 +0200

    feat: 🎸 /qrcode page

---
+++ app/qrcode/page.tsx
@@ -0,0 +1,9 @@
+export default function QRCode() {
+  return (
+    <>
+      <div className='flex flex-col items-center'>
+        <p className='text-md px-4 text-justify mt-4'>En construction</p>
+      </div>
+    </>
+  );
+}
